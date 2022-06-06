cb0e62ea47777033027053458c15a66b7f0f8c53
Piotr Kolodziejski <piotr.kolodziejski00@hotmail.com>
2022-06-06 18:57:45 +0200

    Make basic classes. Add simple Login and Register windows

---
+++ ChessTournaments/ViewModel/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows;
+using ChessTournaments.Model;
+
+namespace ChessTournaments.ViewModel
+{
+    using View;
+    public class LoginViewModel
+    {
+        private ICommand goToRegisterScreen;
+        public ICommand GoToRegisterScreenEvent => goToRegisterScreen ?? (goToRegisterScreen =
+            new RelayCommand(
+                o =>
+                {
+                    RegisterScreen registerScreen = new RegisterScreen();                    
+                    LoginScreen loginScreen = o as LoginScreen;
+                    registerScreen.Show();
+                    loginScreen.Close();
+
+                },
+                null));
+
+    }
+}
---
+++ ChessTournaments/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using ChessTournaments.Model;
+
+namespace ChessTournaments.ViewModel
+{
+    using View;
+    public class RegisterViewModel
+    {
+        private ICommand goToLoginScreen;
+        public ICommand GoToLoginScreenEvent => goToLoginScreen ?? (goToLoginScreen =
+            new RelayCommand(
+                o =>
+                {
+                    LoginScreen loginScreen = new LoginScreen();
+                    RegisterScreen registerScreen = o as RegisterScreen;
+                    loginScreen.Show();
+                    registerScreen.Close();
+
+                },
+                null));
+    }
+}
