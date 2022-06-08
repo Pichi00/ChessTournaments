346c26765df657776a65c101110ae9317644ed8e
Piotr Kołodziejski <piotr.kolodziejski00@hotmail.com>
2022-06-08 14:04:25 +0200

    Add DBConnection

---
+++ ChessTournaments/DAL/Encje/Uzytkownik.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTournaments.DAL.Encje
+{
+    class Uzytkownik
+    {
+        public enum TypyKont {ORGANIZATOR, ZAWODNIK}
+
+        #region Własności
+        public string Login { get; set; }
+        public string Haslo { get; set; }
+        public TypyKont TypKonta { get; set; }
+        #endregion
+
+        #region Konstruktory
+
+        #endregion
+    }
+}
